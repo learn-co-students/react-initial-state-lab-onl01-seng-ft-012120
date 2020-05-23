8c689e2ea9a7db3b3642a38e1bd53085e1e48d78
Harmandeep S. Mand <harmandeepmand.hm@gmail.com>
2020-05-23 01:26:53 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,31 @@
-// your Bomb code here!
+import React from 'react'
+
+class Bomb extends React.Component {
+
+    constructor(props) { 
+        super()
+        this.state = { secondsLeft: props.initialCount }
+    }
+
+    tick() {
+        this.setState(state => ({
+          secondsLeft: state.secondsLeft - 1
+        }));
+      }
+
+    componentDidMount() {
+        this.interval = setInterval(() => this.tick(), 1000);
+    }
+    
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    render(){
+        return (
+            <h1>{this.state.secondsLeft == 0 ? "Boom!" : this.state.secondsLeft+" seconds left before I go boom!"}</h1>
+        );
+    }
+}
+
+export default Bomb;
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,10 @@
-// your ImageSlider code here!
+import React from 'react'
+
+export default class ImageSlider extends React.Component {
+    state = {currentSlideIndex: 0}
+    render(){
+        return (
+        <h1>I am on slide {this.state.currentSlideIndex}</h1>
+        );
+    }
+}
